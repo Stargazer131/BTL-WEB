e81f31912f8045ddd20de26cfb2b312c042080e6
HAO <naotomori131@gmail.com>
2023-05-10 20:13:58 +0700

    fix some name

--- src/main/resources/static/script/main/header.js
+++ src/main/resources/static/script/main/header.js
@@ -2,7 +2,7 @@ window.onscroll = function() {myFunction()}
 
 function myFunction() {
     var header = document.querySelector('.header');
-    if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    if(document.documentElement.scrollTop > 300) {
         header.classList.add('header-background');
     }
     else {
--- src/main/resources/static/script/main/headerShort.js
+++ src/main/resources/static/script/main/headerShort.js
@@ -2,7 +2,7 @@ window.onscroll = function() {myFunction()}
 
 function myFunction() {
     var header = document.querySelector('.header');
-    if(document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    if(document.documentElement.scrollTop > 150) {
         header.classList.add('header-background');
     }
     else {
